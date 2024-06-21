0dd949e7ff2b93da6dd3ad1532b50b4db1b8cb72
Lzxuan03 <102457979+Lzxuan03@users.noreply.github.com>
2024-06-21 09:43:30 +0000

    ADD

--- bin/my_classes.dart
+++ bin/my_classes.dart
@@ -1,10 +1,32 @@
+class Spacecraft {
+  String name;
+  DateTime? launchDate;
+
+  // Read-only non-final property
+  int? get launchYear => launchDate?.year;
+
+  // Constructor, with syntactic sugar for assignment to members.
+  Spacecraft(this.name, this.launchDate) {
+    // Initialization code goes here.
+  }
+
+  // Named constructor that forwards to the default one.
+  Spacecraft.unlaunched(String name) : this(name, null);
+
+  // Method.
+  void describe() {
+    print('Spacecraft: $name');
+    // Type promotion doesn't work on getters.
+    if (launchDate != null) {
+      int years = DateTime.now().difference(launchDate!).inDays ~/ 365;
+      print('Launched: $launchYear ($years years ago)');
+    } else {
+      print('Unlaunched');
+    }
+  }
+}
+
 void main() {
-  var name = 'Voyager I';
-  var year = 1977;
-  var antennaDiameter = 3.7;
-  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
-  var image = {
-    'tags': ['saturn'],
-    'url': '//path/to/saturn.jpg'
-  };
+  Spacecraft space = new Spacecraft('Voyager 1', DateTime(1977, 9, 5));
+  space.describe();  
 }
--- bin/my_control-flow-statements.dart
+++ bin/my_control-flow-statements.dart
@@ -1,4 +1,12 @@
 void main() {
+  var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
   if (year >= 2001) {
     print('21st century');
   } else if (year >= 1901) {
